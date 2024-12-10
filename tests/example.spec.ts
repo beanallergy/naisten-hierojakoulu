59736a2f28c378988926635ac14407d5a4b52296
Thao Nguyen <13851786+beanallergy@users.noreply.github.com>
2024-12-10 13:22:05 +0200

    feat: print out all hierojat names

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -13,21 +13,21 @@ const SEQUENCES: DropdownSequence[] = [
     tyyppi: 'Hieronta (koulutetut hierojat)',
     palvelu: '25 min hieronta (koul.hier.)',
   },
+  /*
   {
     toimipiste: 'LAHTI - Opiskelijahieronta (Karjalankatu 45)',
-    tyyppi: 'Opiskelijahieronta',
+    tyyppi: 'Opiskelijahieronta', // TODO regex matches 3 elements here
     palvelu: '25 min hieronta (opiskelija)',
   }
+  */
 ];
-for (const seq of SEQUENCES) {
-  for (const key of Object.keys(seq) as (keyof DropdownSequence)[]) {
-    console.log(seq[key]);
-  }
-}
+const TARJOAJA = 'Valitse palveluntarjoaja';
+const SEPARATOR = '---------------------------------';
 
 test('Hierojakoulu ajanvaraus', async ({ page }) => {
   const X_SYMBOL = `×`;
   for (const seq of SEQUENCES) {
+    console.log(SEPARATOR);
     await page.goto(URL);
     await expect(page).toHaveTitle(/Ajanvaraus.*Lahden.*hierojakoulu/i);
     await page.waitForLoadState('domcontentloaded');
@@ -37,14 +37,28 @@ test('Hierojakoulu ajanvaraus', async ({ page }) => {
     expect(await allDropdowns.count()).toEqual(4);
 
     for (const key of Object.keys(seq) as (keyof DropdownSequence)[]) {
-      const dropdownText = seq[key];
-      await page.locator('td-select').filter({ hasText: dropdownText }).locator('a').click();
-      await page.getByRole('menuitem', { name: dropdownText }).click();
-      const dropdownSelected = page.getByText(`${dropdownText} ${X_SYMBOL}`);
+      const selectedText = seq[key];
+      await page.locator('td-select').filter({ hasText: selectedText }).locator('a').click();
+      await page.getByRole('menuitem', { name: selectedText }).click();
+      const dropdownSelected = page.getByText(`${selectedText} ${X_SYMBOL} `);
       expect(dropdownSelected).toBeVisible();
-      console.log(`Valittu ${key}: ${dropdownText}`);
+      console.log(`Valittu ${key}: ${selectedText}`);
     }
+    // last dropdown
+    const hierojat = page.locator(`label:has-text('${TARJOAJA}') + td-select div ul li`);
+    const count = await hierojat.count();
+    expect(count).toBeGreaterThan(1);
+    for (let i = 0; i < count; i++) {
+      const hierojanNimi = (await hierojat.nth(i).innerText());
+      if (hierojanNimi.length === 0) {
+        continue;
+      }
+      //await hierojat.nth(i).click();
+      console.log(`Valittu hieroja: ${hierojanNimi}`);
+      //const hierojaSelected = page.getByText(`${hierojanNimi} ${X_SYMBOL} `);
+      //expect(hierojaSelected).toBeVisible();
+    }
+    console.log(SEPARATOR);
   }
-
   await page.close();
 });
