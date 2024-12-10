5deb73cbb48a67810197b86e800a7156aeda4805
Thao Nguyen <13851786+beanallergy@users.noreply.github.com>
2024-12-10 13:22:05 +0200

    fix: select hieroja works now

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosError } from 'axios';
 import * as fs from 'fs';
 import Papa from 'papaparse';
 
@@ -22,7 +22,6 @@ const SEQUENCES: DropdownSequence[] = [
     palvelu: '25 min hieronta (opiskelija)',
   }
 ];
-const TARJOAJA = 'Valitse palveluntarjoaja';
 const SEPARATOR = '---------------------------------';
 
 function readEtunimetCSV(): string[] {
@@ -55,33 +54,6 @@ function isWomanName(name: string, womenNames: string[]): boolean {
   return womenNames.includes(etunimi);
 }
 
-test.skip('Hierojakoulu: calling APIs - NOT WORKING BECAUSE OF CORS', async () => {
-  const company = await axios.get(`${API_BASE_URL}/company/lahdenhierojakoulu?include=extensionsettings,branches`);
-  expect(company.status).toBe(200);
-  expect(company.data).toHaveProperty('data.attributes.branches');
-  const allBranches: any[] = company.data.data.attributes.branches;
-  const branchToLookup = SEQUENCES[0].toimipiste;
-  const BRANCH_ID = allBranches.filter((branch) => branch.name === branchToLookup)[0]['branch_id'];
-  let reservationFilter: any;
-  try {
-    console.info(`Fetching reservationfilter API with branch ${branchToLookup} id=${BRANCH_ID} (looked up from /company API)`);
-    reservationFilter = await axios.get(
-      `${API_BASE_URL}/reservationfilter/?branch_id=${BRANCH_ID}`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Referer": `${WEB_URL}?branch_id=${BRANCH_ID}`,
-          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
-        }
-      }
-    );
-  } catch (error: AxiosError | any) {
-    console.error('Failed to fetch reservationfilter API with headers:', (error as AxiosError).request?._header, 'response headers:', (error as AxiosError).response?.headers);
-    throw(`Failed to fetch reservationfilter API: ${(error as AxiosError).response?.status} ${(error as AxiosError).response?.statusText}`);
-  }
-  console.log('reservationfilter API response:', reservationFilter.data);
-});
-
 test('Hierojakoulu: clicking dropdowns', async ({ page }) => {
   const X_SYMBOL = `×`;
   const naistenNimet = readEtunimetCSV();
@@ -93,7 +65,7 @@ test('Hierojakoulu: clicking dropdowns', async ({ page }) => {
     await page.waitForLoadState('domcontentloaded');
     
     const allDropdowns = page.locator('td-select');
-    await allDropdowns.first().waitFor({ state: 'visible', timeout: 5000 });
+    await allDropdowns.first().waitFor({ state: 'visible', timeout: 10000 });
     expect(await allDropdowns.count()).toEqual(4);
 
     for (const key of Object.keys(seq) as (keyof DropdownSequence)[]) {
@@ -105,36 +77,39 @@ test('Hierojakoulu: clicking dropdowns', async ({ page }) => {
       console.log(`Valittu ${key}: ${selectedText}`);
     }
     // last dropdown
-    let selectedHieroja: string = '';
-    const tarjoajaDropdown = page.locator(`label:has-text('${TARJOAJA}') + td-select`);
-    const hierojat = tarjoajaDropdown.locator('div ul li');
-    const count = await hierojat.count();
-    await tarjoajaDropdown.locator('a').click();
-    expect(count).toBeGreaterThan(1);
-    for (let i = 0; i < count; i++) {
-        if (selectedHieroja.length !== 0) {
-          console.log(`Removing previous selection ${selectedHieroja} ...`);
-          const tarjoajaDropdownSelected = page.locator('a').filter({ hasText: `${selectedHieroja} ${X_SYMBOL}` });
-          await tarjoajaDropdownSelected.locator('a').click();
-          expect(tarjoajaDropdownSelected).not.toBeVisible();
-          selectedHieroja = '';
-        }
-        await tarjoajaDropdown.locator('a').click();
+    const valitse_tarjoaja = 'Valitse palveluntarjoaja';
+    const kuka_tahansa = 'Kuka tahansa';
+    let selectedHierojanNimi: string | undefined = undefined;
+    const tarjoajaDropdown = page.locator(`label:has-text('${valitse_tarjoaja}') + td-select`);
+    const hierojienNimet = (await tarjoajaDropdown.locator('div ul li').allInnerTexts()).filter((nimi) => nimi.length > 0);
+    expect(hierojienNimet.length).toBeGreaterThan(1);
 
-        const hierojanNimi = (await hierojat.nth(i).innerText());
-        if (hierojanNimi.length === 0) {
-          continue;
-        }
-        const isWoman = isWomanName(hierojanNimi, naistenNimet);
-        console.log(`Hierojan nimi: ${hierojanNimi} - likely woman:`, isWoman);
-        if (!isWoman) {
-          continue;
-        }
-        await page.getByRole('menuitem', { name: hierojanNimi }).click();
-        console.log(`Valittu palveluntarjoaja: ${hierojanNimi}`);
-        selectedHieroja = hierojanNimi;
+    for (const hierojanNimi of hierojienNimet) {
+      const isWoman = isWomanName(hierojanNimi, naistenNimet);
+      console.log(`Hierojan nimi: ${hierojanNimi} - likely woman:`, isWoman);
+      if (!isWoman) {
+        continue;
+      }
+
+      if (selectedHierojanNimi != null) {
+        const tarjoajaDropdownSelected = page.locator('a').filter({ hasText: `${selectedHierojanNimi} ${X_SYMBOL}` });
+        if (await tarjoajaDropdownSelected.isVisible()) {
+          console.log(`New loop, removing previous selection ${selectedHierojanNimi} ...`);
+          await tarjoajaDropdownSelected.locator('a').click(); // remove existing selection by clicking x
+          expect(tarjoajaDropdownSelected).not.toBeVisible();
+        };
+        selectedHierojanNimi = undefined;
+      }
+
+      await page.locator('span', { hasText: kuka_tahansa }).isVisible();
+      await page.locator('a').filter({ hasText: kuka_tahansa }).click();
+
+      await page.getByRole('menuitem', { name: hierojanNimi }).click();
+      await page.getByRole('heading', { name: hierojanNimi }).isVisible();
+      selectedHierojanNimi = hierojanNimi;
+      console.log(`Valittu palveluntarjoaja: ${hierojanNimi}`);
     }
     console.log(SEPARATOR);
+    await page.reload();
   }
-  await page.close();
 });
