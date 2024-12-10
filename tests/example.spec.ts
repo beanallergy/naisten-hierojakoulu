fdfe7d84cfb764c4301c638fd41f10e0f990e823
Thao Nguyen <13851786+beanallergy@users.noreply.github.com>
2024-12-10 13:22:05 +0200

    data: women names csv from DVV fix: exact match for link in dropdown

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import csv from 'csv-parser';
+import * as fs from 'fs';
 
 //const URL = 'https://hierojakoulu.net/ajanvaraus/';
 const URL = 'https://avoinna24.fi/lahdenhierojakoulu/reservation';
@@ -15,15 +17,45 @@ const SEQUENCES: DropdownSequence[] = [
   },
   {
     toimipiste: 'LAHTI - Opiskelijahieronta (Karjalankatu 45)',
-    tyyppi: 'Opiskelijahieronta', // TODO regex matches 3 elements here
+    tyyppi: 'Opiskelijahieronta',
     palvelu: '25 min hieronta (opiskelija)',
   }
 ];
 const TARJOAJA = 'Valitse palveluntarjoaja';
 const SEPARATOR = '---------------------------------';
 
+function readEtunimetCSV(): string[] {
+  const filePath = './data/etunimitilasto-2024-08-05-dvv-naiset-kaikki.csv';
+  const naistenEtunimet: string[] = [];
+
+  fs.createReadStream(filePath)
+    .pipe(csv({ separator: ';' }))
+    .on('data', (row: any) => {
+      const name = row['Etunimi'];
+      if (name == null || name.length === 0) {
+        return;
+      }
+      naistenEtunimet.push(name);
+    })
+    .on('end', () => {
+      if (naistenEtunimet.length === 0) {
+        throw('Failed to process CSV file: 0 rows parsed');
+      }
+      console.log(`CSV file processed successfully. Checking against ${naistenEtunimet.length} women names in Finland`);
+    });
+  return naistenEtunimet;
+};
+
+function isWomanName(name: string, womenNames: string[]): boolean {
+  const etunimi = name.trimStart().split(' ')[0];
+  console.log(`Checking first name ${etunimi}`);
+  return womenNames.includes(etunimi);
+}
+
 test('Hierojakoulu ajanvaraus', async ({ page }) => {
   const X_SYMBOL = `×`;
+  const naistenNimet = readEtunimetCSV();
+
   for (const seq of SEQUENCES) {
     console.log(SEPARATOR);
     await page.goto(URL);
@@ -52,7 +84,7 @@ test('Hierojakoulu ajanvaraus', async ({ page }) => {
         continue;
       }
       //await hierojat.nth(i).click();
-      console.log(`Valittu hieroja: ${hierojanNimi}`);
+      console.log(`Hierojan nimi: ${hierojanNimi} - isWomanName=${isWomanName(hierojanNimi, naistenNimet)}`);
       //const hierojaSelected = page.getByText(`${hierojanNimi} ${X_SYMBOL} `);
       //expect(hierojaSelected).toBeVisible();
     }
